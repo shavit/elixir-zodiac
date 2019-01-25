d88dc275da0d40cb3a408b301efa93d400aebf8f
shavit <shavit@users.noreply.github.com>
2019-01-25 13:38:17 -0500

    Prepare for release
    • Add .travis.yml
    • Remove the empty dependency list

--- mix.exs
+++ mix.exs
@@ -6,8 +6,7 @@ defmodule Zodiac.MixProject do
       app: :zodiac,
       version: "0.1.0",
       elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      start_permanent: Mix.env() == :prod
     ]
   end
 
@@ -15,14 +14,6 @@ defmodule Zodiac.MixProject do
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
